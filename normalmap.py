0e46351f7ce62316fc76e6ed0f6187a7246d3e18
lynnpepin <lynnpepin@tutanota.com>
2021-09-10 14:21:36 -0400

    Gitignore

--- normalmap.py
+++ normalmap.py
@@ -14,11 +14,6 @@ key_angles = np.union1d(
 )
 
 
-base_angles = np.array(
-  [0, np.pi/12, np.pi/8, np.pi/6]
-)
-
-
 def normal_from_angles(theta, phi):
     '''Get a unit vector in R3 described by theta and phi,
     where theta is in the xy plane, (range -pi to pi)
@@ -32,17 +27,27 @@ def normal_from_angles(theta, phi):
     :returns:
     :rtype:
     '''
-  #theta in xy plane,
-  #-pi to pi
-  #phi from 0 to pi/2
-  zz = np.cos(phi)
-  rr = np.sin(phi)
-  xx = rr*np.cos(theta)
-  yy = rr*np.sin(theta)
-  return np.array([xx, yy, zz])
+    #theta in xy plane,
+    #-pi to pi
+    #phi from 0 to pi/2
 
-# todo from here
+    # zz is our 
+    zz = np.cos(phi)
+    rr = np.sin(phi)
+    xx = rr*np.cos(theta)
+    yy = rr*np.sin(theta)
+    return np.array([xx, yy, zz])
 
+def _test_normal_from_angles():
+  # This should return without an AssertionError
+  # And it does :)
+  for theta in key_angles:
+    for phi in key_angles:
+      vec = normal_from_angles(theta, phi)
+      magnitude = np.linalg.norm(vec)
+      assert (magnitude - 1.0) < 0.000001, f"Oh no! Angle {vec} has magnitude {magnitude}"
+
+'''
 # now we convert a normal vector to rgb
 def normal2rgb(nn):
   return ((nn + 1)*(255/2)).round().astype(np.int)
@@ -57,7 +62,4 @@ for phi in key_angles:
     print(rgb2hex(*normal2rgb(get_normal(theta, phi))))
 
     #`print(f"theta: {(phi*np.pi/180).round()}")`
-    
-
-
-  
+'''
