1f0ae3b7ec0c79d3aedb2c62b59d6cb0bad076a9
lynnpepin <lynnpepin@tutanota.com>
2021-09-27 19:13:15 -0400

    Some new normal work

--- loa40xx/Player.gd
+++ loa40xx/Player.gd
@@ -6,6 +6,9 @@ extends KinematicBody2D
 # var b = "text"
 var  SPEED = 256
 var DIRECTION = Vector2(0,0)
+var LIGHT_DIST = 16
+var LIGHT_SPEED = 2
+var timer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,9 +18,21 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
+	timer += delta
 	DIRECTION.x = (int(Input.is_action_pressed("pad_right")) - int(Input.is_action_pressed("pad_left")))
 	DIRECTION.y = (int(Input.is_action_pressed("pad_down")) - int(Input.is_action_pressed("pad_up")))
 	move_and_slide(DIRECTION.normalized()*SPEED)
+	
+	
+	
+	$LightRed.position.x = int(LIGHT_DIST*cos(timer*LIGHT_SPEED))
+	$LightRed.position.y = int(LIGHT_DIST*sin(timer*LIGHT_SPEED))
+	
+	$LightBlue.position.x = int(LIGHT_DIST*cos(timer*LIGHT_SPEED*3+PI/3))
+	$LightBlue.position.y = int(LIGHT_DIST*sin(timer*LIGHT_SPEED*3+PI/3))
+	
+	$LightGreen.position.x = int(LIGHT_DIST*cos(timer*LIGHT_SPEED*2+2*PI/3))
+	$LightGreen.position.y = int(LIGHT_DIST*sin(timer*LIGHT_SPEED*2+2*PI/3))
 
 
 
